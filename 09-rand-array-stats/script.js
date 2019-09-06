0405a6a0916d1ef6989e8f660c4d8e4b0f108da3
Jeroen <jrn.hendrickx@gmail.com>
2019-09-06 15:01:46 +0200

    All exercises from chapter 5

---
+++ 09-rand-array-stats/script.js
@@ -0,0 +1,28 @@
+/* becode/javascript
+ *
+ * /05-arrays/09-rand-array-stats/script.js - 5.9: tableau aléatoire & statistiques
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function () {
+        let data = document.querySelectorAll("td");
+        let arr = [];
+        for (i = 0; i < 10; i++) {
+            let random = Math.floor(Math.random()*100) + 1;
+            arr.push(random);
+            data[i].innerHTML = arr[i];
+        }
+        document.getElementById("min").innerHTML = Math.min(...arr);
+        document.getElementById("max").innerHTML = Math.max(...arr);
+        document.getElementById("sum").innerHTML = arr.reduce((a, b) => a + b, 0);
+        document.getElementById("average").innerHTML = arr.reduce((a,b) => a + b, 0) / arr.length;
+    })
+
+})();
